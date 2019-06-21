07c08a3865e63e3761dcb873fd2504d4d7d40976
Andrew Morris <amorris28@gmail.com>
2019-06-20 18:20:54 -0700

    Replace rarefied with VST ASV table

--- R/gab_combine_data.R
+++ R/gab_combine_data.R
@@ -1,21 +1,23 @@
 library(tidyverse)
 library(morris)
 
-gen_attr_table <- read_csv('../output/gab_gen_attr_table.csv')
-
+# Methanogen metadata
+gen_attr_table <- read.csv('../output/gab_gen_attr_table.csv', stringsAsFactors = FALSE)
 gen_attr_table$Site <- paste0(gen_attr_table$Site, 
 				reverse_substr(gen_attr_table$Sample, 3, 3))
+
+# Methanotrophy metadata
 troph_attr_table <- read.csv('../output/gab_troph_attr_table.csv', stringsAsFactors=F)
 troph_attr_table$Site <- paste0(troph_attr_table$Site, 
 				reverse_substr(troph_attr_table$Sample, 3, 3))
 ## Rarefied ASV Table
-#asv_table <- read.csv('../output/gab_rare_asv_table.csv')
+#rare_asv_table <- read.csv('../output/gab_rare_asv_table.csv')
 
 ## DESeq2 variance stabilized asv table
-asv_table <- read_csv('../output/vst_asv_table.csv')
+vst_asv_table <- read.csv('../output/vst_asv_table.csv', stringsAsFactors = FALSE)
 
 ## Raw sequence counts
-pa_asv_table <- read_csv('../output/gab_asv_table.csv')
+pa_asv_table <- vst_asv_table #read_csv('../output/gab_asv_table.csv')
 asvs <- pa_asv_table[, -1] 
 ## Presence Absence
 asvs[asvs>0] <- 1
@@ -26,14 +28,14 @@ pa_asv_table[, -1] <- asvs
 geodist <- read.table('../output/geodist.tsv', stringsAsFactors = FALSE)
 
 # Export
-troph_total_rare <- left_join(troph_attr_table, geodist, by = "Site")
-troph_total_rare <- inner_join(troph_total_rare, asv_table, by = "Sample")
+troph_total_vst <- left_join(troph_attr_table, geodist, by = "Site")
+troph_total_vst <- inner_join(troph_total_vst, vst_asv_table, by = "Sample")
 #troph_total_rare <- troph_total_rare[troph_total_rare$Wetland == 'Upland', ]
-write_csv(troph_total_rare, '../output/gab_all_rare_troph.csv')
+write_csv(troph_total_vst, '../output/gab_all_vst_troph.csv')
 
-gen_total_rare <- left_join(gen_attr_table, geodist, by = "Site")
-gen_total_rare <- inner_join(gen_total_rare, asv_table, by = "Sample")
-write_csv(gen_total_rare, '../output/gab_all_rare_gen.csv')
+gen_total_vst <- left_join(gen_attr_table, geodist, by = "Site")
+gen_total_vst <- inner_join(gen_total_vst, vst_asv_table, by = "Sample")
+write_csv(gen_total_vst, '../output/gab_all_vst_gen.csv')
 
 troph_total_pa <- left_join(troph_attr_table, geodist, by = "Site")
 troph_total_pa <- inner_join(troph_total_pa, pa_asv_table, by = "Sample")
