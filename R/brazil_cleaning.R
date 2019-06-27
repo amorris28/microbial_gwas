280b1f0613c731787e33b30c75f99b85f391080a
Andrew Morris <amorris28@gmail.com>
2019-06-27 16:14:21 -0700

    Add VST to Brazil

--- R/brazil_cleaning.R
+++ R/brazil_cleaning.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+library(DESeq2)
 
 ## Cleaning up metadata
 metadata <- read_tsv('../data/brazil/Dimensions_meta_data.txt')
@@ -11,17 +12,29 @@ metadata <- as.data.frame(metadata)
 metadata <- sapply(metadata,function(x) gsub(",",".",x))
 metadata <- as_tibble(metadata)
 metadata <- mutate_at(metadata, vars(pH:Error_N2O), as.numeric)
-as.data.frame(metadata)
-
 ## ASVs
-asvs <- read_tsv('../data/brazil/16S_dada2_table_RDP_tax.txt')
-colnames(asvs)
-taxa_abund <- 
+asvs <- as.data.frame(data.table::fread('../data/brazil/16S_dada2_table_RDP_tax.txt'))
+asvs <-
   asvs %>% 
-  mutate(OTU_ID = paste0("asv_", OTU_ID)) %>% 
-  select(-ConsensusLineage) %>% 
+  select(-ConsensusLineage)
+asv_mat <- asvs[,-1]
+rownames(asv_mat) <- asvs$OTU_ID
+col <- data.frame(site = colnames(asv_mat))
+dds <- DESeqDataSetFromMatrix(countData = asv_mat, design = ~ 1, colData = col)
+dds <- estimateSizeFactors(dds, type = 'poscounts')
+dds <- estimateDispersions(dds)
+vsd <- varianceStabilizingTransformation(dds)
+varstab_commat <- assay(vsd)
+vsd_data <- 
+	cbind(tibble(asv = rownames(varstab_commat)), as_tibble(varstab_commat)) %>%
+  mutate(asv = paste('asv', asv, sep = "_")) 
+
+taxa_abund <- 
+  vsd_data %>% 
   gather(Sample_ID, Abundance, 2:75) %>% 
-  spread(OTU_ID, Abundance)
+  spread(asv, Abundance)
+taxa_abund[taxa_abund < 0] <- 0
+
 gps <- read.table('../data/brazil/Dimensions_GPS_dec_deg.txt', header = FALSE)
 colnames(gps) <- c('Sample_ID', 'Lat', 'Long')
 
@@ -30,5 +43,5 @@ all_data <-
   right_join(metadata, by = 'Sample_ID') %>% 
   right_join(taxa_abund, by = 'Sample_ID')
 
-all_data$library_size <- rowSums(taxa_abund[, -1])
-write_tsv(all_data, '../output/dim_cleaned_data.tsv')
+#all_data$library_size <- rowSums(taxa_abund[, -1])
+write_tsv(all_data, '../output/brazil_cleaned_data.tsv')
