1dd8dcd8339a45da806bd206a4ddb9772f3d0f92
Andrew Morris <amorris28@gmail.com>
2019-08-22 17:01:20 -0700

    update library and rerun divnet

--- Archive/divnet.R
+++ Archive/divnet.R
@@ -8,6 +8,7 @@ a <- as(otu_table(physeq), 'matrix')
 a[a > 1] <- 1
 ref_otu <- colnames(a)[colSums(a) == max(colSums(a))]
 
-asv_div <- divnet(physeq, ncores = 28, base = ref_otu)
+asv_div <- divnet(physeq, ncores = 3, base = ref_otu, network = "diagonal",
+tuning =  list(EMiter = 6, EMburn = 3, MCiter = 10, MCburn = 5))
 
 saveRDS(asv_div, '../output/divnet_asv.rds')
